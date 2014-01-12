1408cfa40e96faf7cfaa0e304558feab0d77c804
Jannes Jeising <jannes@jeising.net>
2014-01-12 22:35:29 +0100

    Updated to PHP 5.5.8, cleanup, added xdiff
    Also add use homebrew curl, version bundled with Mac OS X Mavericks has
    issues with certificates.

--- php.rb
+++ php.rb
@@ -25,34 +25,43 @@ class PhpMemcached < Formula
 end
 
 class PhpSsh2 < Formula
-  homepage ''
+  homepage 'http://pecl.php.net/package/ssh2'
   url 'http://pecl.php.net/get/ssh2-0.12.tgz'
   sha1 'b86a25bdd3f3558bbcaaa6d876309fbbb5ae134d'
+end
+
+class PhpXdiff < Formula
+  homepage 'http://pecl.php.net/package/xdiff'
+  url 'http://pecl.php.net/get/xdiff-1.5.2.tgz'
+  sha1 '5baa9716fc951d2e3f4e8e73b491493f7d3c3c80'
 end
 
 class PhpZmq < Formula
   homepage 'https://github.com/mkoppanen/php-zmq'
   url 'http://pecl.php.net/get/zmq-1.1.1.tgz'
-  sha1 ''
+  sha1 '835e8a23ef465ce2b404417bba05b27db425eec0'
 end
 
 class Php < Formula
   homepage 'http://php.net/'
-  url 'http://de.php.net/distributions/php-5.5.5.tar.gz'
-  sha1 'f08e6c2a5e1e171c1808ba2da8b8102a778f0690'
+  url 'http://de.php.net/distributions/php-5.5.8.tar.gz'
+  sha1 '19af9180c664c4b8f6c46fc10fbad9f935e07b52'
   
   head 'https://svn.php.net/repository/php/php-src/trunk', :using => :svn
   
   depends_on 'pkg-config' => :build
   
+  depends_on 'autoconf'
+  depends_on 'curl'
+  depends_on 'freetype'
   depends_on 'gettext'
   depends_on 'icu4c'
+  depends_on 'imagemagick'
   depends_on 'imap-uw'
   depends_on 'jpeg'
+  depends_on 'libxdiff'
   depends_on 'libxml2'
   depends_on 'mcrypt'
-  depends_on 'imagemagick'
-  depends_on 'autoconf'
   depends_on 'zeromq'
   
   # mysql/pgsql dependencies?
@@ -68,57 +77,60 @@ class Php < Formula
     ext = Pathname.new(pwd) + 'ext/'
     
     args = [
-      "--prefix=#{prefix}",
-      "--disable-debug",
       "--localstatedir=#{var}",
+      "--mandir=#{man}",
+      "--prefix=#{prefix}",
+      
       "--sysconfdir=#{config_path}",
       "--with-config-file-path=#{config_path}",
-      "--with-iconv-dir=/usr",
-      "--enable-exif",  
-      "--enable-soap",
-      "--enable-sockets",
-      "--enable-zip",
-      "--enable-shmop",
-      "--enable-sysvsem",
-      "--enable-sysvshm",
-      "--enable-sysvmsg",
-      "--enable-mbstring",
+      
+      "--disable-debug",
       "--disable-mbregex",
-      "--enable-zend-signals",
+      "--enable-apcu",
       "--enable-bcmath",
       "--enable-calendar",
-      "--with-openssl=/usr",
-      "--with-zlib=/usr",
+      "--enable-ctype",
+      "--enable-exif",  
+      "--enable-fpm",
+      "--enable-gd-native-ttf",
+      "--enable-igbinary",
+      "--enable-mbstring",
+      "--enable-pcntl",
+      "--enable-shmop",
+      "--enable-soap",
+      "--enable-sockets",
+      "--enable-sysvmsg",
+      "--enable-sysvsem",
+      "--enable-sysvshm",
+      "--enable-zend-signals",
+      "--enable-zip",
       "--with-bz2=/usr",
+      "--with-curl=#{Formula.factory('curl').prefix}",
+      "--with-freetype-dir=#{Formula.factory('freetype').prefix}",
+      "--with-gd",
+      "--with-gettext=#{Formula.factory('gettext').prefix}",
+      "--with-iconv-dir=/usr",
+      "--with-imagick",
+      "--with-jpeg-dir=#{Formula.factory('jpeg').prefix}",
       "--with-ldap",
       "--with-ldap-sasl=/usr",
-      "--with-xmlrpc",
-      "--with-libxml-dir=#{Formula.factory('libxml2').prefix}",
-      "--with-xsl=/usr",
-      "--with-curl=/usr",
-      "--with-gd",
-      "--enable-gd-native-ttf",
-      "--with-freetype-dir=/usr/X11",
-      "--with-mcrypt=#{Formula.factory('mcrypt').prefix}",
-      "--with-jpeg-dir=#{Formula.factory('jpeg').prefix}",
-      "--with-png-dir=/usr/X11",
-      "--with-gettext=#{Formula.factory('gettext').prefix}",
-      "--with-snmp=/usr",
-      "--mandir=#{man}",
       "--with-libedit",
-      "--enable-ctype",
-      "--enable-apcu",
-      "--with-imagick",
-      "--enable-igbinary",
-      "--with-ssh2",
+      "--with-libxml-dir=#{Formula.factory('libxml2').prefix}",
+      "--with-mcrypt=#{Formula.factory('mcrypt').prefix}",
       "--with-mysql-sock=/tmp/mysql.sock",
-      "--with-mysqli=mysqlnd",
       "--with-mysql=mysqlnd",
+      "--with-mysqli=mysqlnd",
+      "--with-openssl=/usr",
       "--with-pdo-mysql=mysqlnd",
       "--with-pdo-pgsql",
-      "--enable-fpm",
-      "--with-zmq",
-      "--enable-pcntl"
+      "--with-png-dir=/usr/X11",
+      "--with-snmp=/usr",
+      "--with-ssh2",
+      "--with-xdiff",
+      "--with-xmlrpc",
+      "--with-xsl=/usr",
+      "--with-zlib=/usr",
+      "--with-zmq"
     ]
     
     PhpApcu.new.brew { ext.install Hash[Dir['*'].map {|x| [x, 'apcu/'] if File.directory? x}] }
@@ -126,9 +138,9 @@ class Php < Formula
     PhpImagick.new.brew { ext.install Hash[Dir['*'].map {|x| [x, 'imagick/'] if File.directory? x}] }
     PhpMemcached.new.brew { ext.install Hash[Dir['*'].map {|x| [x, 'memcached/'] if File.directory? x}] }
     PhpSsh2.new.brew { ext.install Hash[Dir['*'].map {|x| [x, 'ssh2/'] if File.directory? x}] }
+    PhpXdiff.new.brew { ext.install Hash[Dir['*'].map {|x| [x, 'xdiff/'] if File.directory? x}] }
     PhpZmq.new.brew { (ext/'zmq').install Dir['*'] }
     
-    # build new configure to include extensions above
     system "rm configure"
     system "./buildconf --force"
     
