303666eee476633b85bd58b44d73acd5a6082c42
Jannes Jeising <jannes@jeising.net>
2015-02-17 19:32:55 +0100

    Removed XHP
    Extension is unmaintained and will not come back.

--- php.rb
+++ php.rb
@@ -27,9 +27,6 @@ class Php < Formula
   
   # mysql/pgsql dependencies?
   
-  # XHP dependency
-  # depends_on 're2c'
-  
   resource 'apcu' do
     url 'http://pecl.php.net/get/apcu-4.0.6.tgz'
     sha1 'f4841f20b333638381b3180ffa1f66b69de1de0f'
@@ -64,11 +61,6 @@ class Php < Formula
     url 'http://pecl.php.net/get/xdiff-1.5.2.tgz'
     sha1 '5baa9716fc951d2e3f4e8e73b491493f7d3c3c80'
   end
-  
-  #resource 'xhp' do
-  #  url 'https://github.com/facebook/xhp/archive/1.5.zip'
-  #  sha1 'b7df94608da6ae84b0f68e8e986f586f49f5851e'
-  #end
   
   resource 'xhprof' do
     url 'http://pecl.php.net/get/xhprof-0.9.4.tgz'
@@ -111,7 +103,6 @@ class Php < Formula
       "--enable-sysvmsg",
       "--enable-sysvsem",
       "--enable-sysvshm",
-      # "--enable-xhp",
       "--enable-xhprof",
       "--enable-zend-signals",
       "--enable-zip",
@@ -151,7 +142,6 @@ class Php < Formula
     end
     
     resource("xhprof").stage { (ext/"xhprof").install Dir["xhprof*/extension/*"] }
-    # resource("xhp").stage { (ext/"xhp").install Dir["*"] }
     
     system "rm configure"
     system "./buildconf --force"
