0a25607478dce2cd8a88c4e17e8d1a99f4c931f2
Jannes Jeising <jannes@jeising.net>
2015-06-13 00:11:04 +0200

    Updated xdebug to 2.3.2

--- php-xdebug.rb
+++ php-xdebug.rb
@@ -2,9 +2,11 @@ require 'formula'
 
 class PhpXdebug < Formula
   homepage 'http://xdebug.org/'
-  url 'http://xdebug.org/files/xdebug-2.2.5.tgz'
-  sha1 '62d388e07a45cab9eee498e7905c92a7e5d023cc'
+  url 'http://xdebug.org/files/xdebug-2.3.2.tgz'
+  sha256 'f875d0f8c4e96fa7c698a461a14faa6331694be231e2ddc4f3de0733322fc6d0'
 
+  depends_on 'autoconf' => :build
+  
   depends_on 'php'
 
   def install
